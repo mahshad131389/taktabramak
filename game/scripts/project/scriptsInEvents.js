e36929fafa5ff590899f9b8a217d0e6a85f08eb9
mahshad131389 <m.khazaei8086@gmail.com>
2025-07-29 11:54:37 +0330

    Add files via upload

---
+++ game/scripts/project/scriptsInEvents.js
@@ -0,0 +1,15 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameSheet_Event239_Act2(runtime, localVars)
+	{
+		console.log(localVars.vValue)
+		window.parent.postMessage(localVars.vValue, '*')
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
